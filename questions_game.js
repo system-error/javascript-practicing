2972ce65094b69cab6789252e649eb6d615b9481
Christos Dousmanopoulos <system-error@users.noreply.github.com>
2018-12-03 16:50:23 +0200

    A new challenge game with questions
    It hasn't finished yet

---
+++ questions_game.js
@@ -0,0 +1,27 @@
+var Question = function(question, answers, coranswer) {
+    this.question = question;
+    this.answers = answers;
+    this.coranswer = coranswer;
+}
+
+Question.prototype.questions = function() {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+        console.log(i + ':' + this.answers[i]);
+    }
+}
+
+var question1 = new Question('Is the number 10 even or odd number?', ['odd', 'even'], 1);
+
+var question2 = new Question('What is the color when the traffic light says to stop? ', ['green', 'orange', 'red'], 2);
+
+var question3 = new Question('How many legs has a dog', [1, 2, 3, 4], 3);
+
+question1.questions();
+
+var allTheAnswers = [question1, question2, question3];
+
+var randomQuestions = Math.floor(Math.random() * allTheAnswers.length);
+
+
+console.log(allTheAnswers[randomQuestions]);
